d23d35769f6b883e650dd24f74d7d2f3adea8a2b
Rishabh <rishuprakash2002@gmail.com>
2022-03-17 12:45:28 +0530

    Backend connected

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,30 +1,59 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { MdEmail } from "react-icons/md";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import Header from "../../components/layout/Header";
 import "./styles.scss";
+import axios from "axios";
 
 const Login = () => {
-  const password = useRef(null);
+  const passwrd = useRef(null);
   const show = useRef(null);
   const hide = useRef(null);
 
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    error: "",
+    loading: false,
+    success: false,
+  });
+  const { email, password, error, loading, success } = values;
+
   //* To show and hide password
   const handlePassword = () => {
-    if (password.current.type === "text") {
-      password.current.type = "password";
+    if (passwrd.current.type === "text") {
+      passwrd.current.type = "password";
       show.current.style.display = "none";
       hide.current.style.display = "";
     } else {
-      password.current.type = "text";
+      passwrd.current.type = "text";
       show.current.style.display = "";
       hide.current.style.display = "none";
     }
   };
 
   //* Show other methods
-  const handleChangeMethods = () => {};
+  // const handleChangeMethods = () => {};
+
+  //* Input field changes
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, [name]: event.target.value });
+  };
+
+  //* Submit form to backend
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:8000/api/login", values)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error.response.data.message);
+        setValues({ ...values, error: error.response.data.message });
+      });
+  };
 
   return (
     <div className="auth">
@@ -37,7 +66,13 @@ const Login = () => {
       <form action="">
         <div className="input">
           <p>Email</p>
-          <input type="email" placeholder="example@gmail.com" />
+          <input
+            type="email"
+            placeholder="example@gmail.com"
+            required
+            onChange={handleChange("email")}
+            value={email}
+          />
           <div className="icons">
             <MdEmail />
           </div>
@@ -46,8 +81,11 @@ const Login = () => {
           <p>Password</p>
           <input
             type="password"
-            ref={password}
+            ref={passwrd}
             placeholder="&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;&#8226;"
+            required
+            onChange={handleChange("password")}
+            value={password}
           />
           <div className="icons" onClick={handlePassword}>
             <span ref={show} style={{ display: "none" }}>
@@ -59,10 +97,10 @@ const Login = () => {
           </div>
         </div>
         <div className="actions">
-          <div className="btn" onClick={handleChangeMethods}>
-            Change Method
-          </div>
-          <button type="submit">Login</button>
+          <div className="btn">Change Method</div>
+          <button type="submit" onClick={handleSubmit}>
+            Login
+          </button>
         </div>
       </form>
 
