0532a7c875fffa1d416723e565ae08ca09ff7572
pawan <pawanr2790@gmail.com>
2022-03-17 12:23:41 +0530

    "password"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
+import Password from "./pages/auth/Password";
 function App() {
   return (
     <div className="App">
       <Login />
       <Register />
+      <Password />
+      
     </div>
   );
 }
